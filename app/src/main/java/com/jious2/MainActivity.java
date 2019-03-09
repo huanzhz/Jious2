4de483b1ada986e912c68feb72a4bfe6c01e17d3
HuanZhang <huanzhz@users.noreply.github.com>
2019-03-09 11:27:14 +0800

    Firebase Sign Out android - Android Appbar Layout drop down list with Options

--- app/src/main/java/com/jious2/MainActivity.java
+++ app/src/main/java/com/jious2/MainActivity.java
@@ -6,6 +6,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TableLayout;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -55,5 +58,34 @@ public class MainActivity extends AppCompatActivity {
     private void SendUserToLoginActivity(){
         Intent loginInent = new Intent(MainActivity.this, LoginActivity.class);
         startActivity(loginInent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_option)
+        {
+            mAuth.signOut();
+            SendUserToLoginActivity();
+        }
+        if(item.getItemId() == R.id.main_settings_option)
+        {
+
+        }
+        if(item.getItemId() == R.id.main_find_friends_option)
+        {
+
+        }
+        return true;
     }
 }
