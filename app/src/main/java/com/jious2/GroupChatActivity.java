db8a5cc36acb75ffcb801935ae587d2fc46d83c2
HuanZhang <huanzhz@users.noreply.github.com>
2019-03-10 16:56:35 +0800

    Create Group Chat Activity

---
+++ app/src/main/java/com/jious2/GroupChatActivity.java
@@ -0,0 +1,37 @@
+package com.jious2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+
+public class GroupChatActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private ImageButton SendMessageButton;
+    private EditText userMessageInput;
+    private ScrollView mScrollView;
+    private TextView displayTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_chat);
+
+        InitializeFeilds();
+    }
+
+    private void InitializeFeilds(){
+        mToolbar = (Toolbar) findViewById(R.id.group_chat_bar_layout);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Group Name");
+
+        SendMessageButton = (ImageButton) findViewById(R.id.send_message_button);
+        userMessageInput = (EditText) findViewById(R.id.input_group_message);
+        displayTextMessage = (TextView) findViewById(R.id.group_chat_text_display);
+        mScrollView = (ScrollView) findViewById(R.id.my_scroll_view);
+    }
+}
