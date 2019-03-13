f73bad57049c1e9660514278cc22591942f1c598
HuanZhang <huanzhz@users.noreply.github.com>
2019-03-13 13:44:58 +0800

    Send Chat Request

--- app/src/main/java/com/jious2/AccountActivity/ProfileActivity.java
+++ app/src/main/java/com/jious2/AccountActivity/ProfileActivity.java
@@ -3,10 +3,14 @@ package com.jious2.AccountActivity;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,27 +23,32 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    private String receiverUserID;
+    private String receiverUserID, senderUserUD, Current_Stats;
 
     private CircleImageView userProfileImage;
     private TextView userProfileName, getUserProfileStatus;
     private Button SendMessageRequestButton;
 
-    private DatabaseReference UserRef;
+    private DatabaseReference UserRef, ChatRequestRef;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mAuth = FirebaseAuth.getInstance();
         UserRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        ChatRequestRef = FirebaseDatabase.getInstance().getReference().child("Chat Requests");
 
         receiverUserID = getIntent().getExtras().get("visit_user_id").toString();
+        senderUserUD = mAuth.getCurrentUser().getUid();
 
         userProfileImage = (CircleImageView) findViewById(R.id.visit_profile_image);
         userProfileName = (TextView) findViewById(R.id.visit_user_name);
         getUserProfileStatus = (TextView) findViewById(R.id.visit_profile_status);
         SendMessageRequestButton = (Button) findViewById(R.id.send_message_request_button);
+        Current_Stats = "new";
 
         RetrieveUserInfo();
     }
@@ -57,6 +66,8 @@ public class ProfileActivity extends AppCompatActivity {
                     Picasso.get().load(userImage).placeholder(R.drawable.profile_image).into(userProfileImage);
                     userProfileName.setText(userName);
                     getUserProfileStatus.setText(userStatus);
+
+                    ManageChatRequests();
                 }
                 else
                 {
@@ -65,6 +76,8 @@ public class ProfileActivity extends AppCompatActivity {
 
                     userProfileName.setText(userName);
                     getUserProfileStatus.setText(userStatus);
+
+                    ManageChatRequests();
                 }
             }
 
@@ -73,5 +86,75 @@ public class ProfileActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void ManageChatRequests(){
+
+        ChatRequestRef.child(senderUserUD)
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(receiverUserID))
+                        {
+                            String request_type = dataSnapshot.child(receiverUserID).child("request_type").getValue().toString();
+
+                            if(request_type.equals("sent"))
+                            {
+                                Current_Stats = "request_sent";
+                                SendMessageRequestButton.setText("Cancel Chat Request");
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+        if(!senderUserUD.equals(receiverUserID))
+        {
+            SendMessageRequestButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    SendMessageRequestButton.setEnabled(false);
+
+                    if(Current_Stats.equals("new"))
+                    {
+                        SendChatRequest();
+                    }
+                }
+            });
+        }
+        else
+        {
+            SendMessageRequestButton.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void SendChatRequest(){
+        ChatRequestRef.child(senderUserUD).child(receiverUserID)
+                .child("request_type").setValue("sent")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful())
+                        {
+                            ChatRequestRef.child(receiverUserID).child(senderUserUD)
+                                    .child("request_type").setValue("received")
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful())
+                                            {
+                                                SendMessageRequestButton.setEnabled(true);
+                                                Current_Stats = "request_sent";
+                                                SendMessageRequestButton.setText("Cancel Chat Request");
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                });
     }
 }
