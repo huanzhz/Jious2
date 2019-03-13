4730724115b94c7e51a9acc636f92cd43cba1e03
HuanZhang <huanzhz@users.noreply.github.com>
2019-03-13 18:01:27 +0800

    Requests Fragment + (accept and cancel btn)

---
+++ app/src/main/java/com/jious2/Fragments/RequestsFragment.java
@@ -0,0 +1,39 @@
+package com.jious2.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jious2.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RequestsFragment extends Fragment {
+
+    private View RequestsFragmentView;
+    private RecyclerView myRequestsList;
+
+    public RequestsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        RequestsFragmentView = inflater.inflate(R.layout.fragment_requests,container,false);
+
+        myRequestsList = (RecyclerView) RequestsFragmentView.findViewById(R.id.chat_requests_list);
+        myRequestsList.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return RequestsFragmentView;
+    }
+
+}
