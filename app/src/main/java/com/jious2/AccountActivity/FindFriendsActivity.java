64da831091cebd6835736826a6375d20aee583b9
HuanZhang <huanzhz@users.noreply.github.com>
2019-03-13 10:56:41 +0800

    Find Friends Activity

---
+++ app/src/main/java/com/jious2/AccountActivity/FindFriendsActivity.java
@@ -0,0 +1,30 @@
+package com.jious2.AccountActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.jious2.R;
+
+public class FindFriendsActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    private RecyclerView FindFriendsRecyclerList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_friends);
+
+        FindFriendsRecyclerList = (RecyclerView) findViewById(R.id.find_friends_recycler_list);
+        FindFriendsRecyclerList.setLayoutManager(new LinearLayoutManager(this));
+
+        mToolbar = (Toolbar) findViewById(R.id.find_friends_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Find Friends");
+    }
+}
